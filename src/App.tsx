f95f2eecddf85d61411cee5098f093b9eb62fbd3
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-10 19:57:51 +0100

    criei o card historico

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import userImagem from "../public/Picture.png";
 import pinGift from "../public/pinGift.png";
 import { HiBadgeCheck } from "react-icons/hi";
 import { IoGiftOutline } from "react-icons/io5";
+import { BsPatchCheck } from "react-icons/bs";
 
 function App() {
   return (
@@ -78,6 +79,7 @@ function App() {
             </span>
           </div>
         </div>
+
         <div className="h-24 rounded-2xl py-4 px-5 bg-gray-100 flex gap-6 items-center justify-between">
           <div className="w-full">
             <h3 className="text-gray-500 text-sm flex gap-2">
@@ -94,6 +96,78 @@ function App() {
           <div className="w-16 h-14 rounded-full bg-gray-200 flex justify-center items-center">
             <img src={pinGift} alt="imagem icon gift" width={32} height={32} />
           </div>
+        </div>
+
+        <div className="flex flex-col gap-4 px-5 pb-5 pt-6 bg-gray-100 rounded-2xl">
+          <div className="flex justify-between">
+            <h2 className="text-gray-500 uppercase font-bold text-sm">
+              Histórico
+            </h2>
+            <span className="text-gray-500 text-xs">7 cortes</span>
+          </div>
+          <div className="h-px bg-gray-300" />
+
+          <ul>
+            <li className="flex justify-between items-center py-2">
+              <div className="space-y-2">
+                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
+                <h3 className="text-gray-500 text-xs">18:30</h3>
+              </div>
+              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
+                <BsPatchCheck className="size-5 text-green-600" />
+              </div>
+            </li>
+
+            <li className="flex justify-between items-center py-2">
+              <div className="space-y-2">
+                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
+                <h3 className="text-gray-500 text-xs">18:30</h3>
+              </div>
+              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
+                <BsPatchCheck className="size-5 text-green-600" />
+              </div>
+            </li>
+
+            <li className="flex justify-between items-center py-2">
+              <div className="space-y-2">
+                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
+                <h3 className="text-gray-500 text-xs">18:30</h3>
+              </div>
+              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
+                <BsPatchCheck className="size-5 text-green-600" />
+              </div>
+            </li>
+
+            <li className="flex justify-between items-center py-2">
+              <div className="space-y-2">
+                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
+                <h3 className="text-gray-500 text-xs">18:30</h3>
+              </div>
+              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
+                <BsPatchCheck className="size-5 text-green-600" />
+              </div>
+            </li>
+
+            <li className="flex justify-between items-center py-2">
+              <div className="space-y-2">
+                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
+                <h3 className="text-gray-500 text-xs">18:30</h3>
+              </div>
+              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
+                <BsPatchCheck className="size-5 text-green-600" />
+              </div>
+            </li>
+
+            <li className="flex justify-between items-center py-2">
+              <div className="space-y-2">
+                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
+                <h3 className="text-gray-500 text-xs">18:30</h3>
+              </div>
+              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
+                <BsPatchCheck className="size-5 text-green-600" />
+              </div>
+            </li>
+          </ul>
         </div>
       </div>
     </div>
