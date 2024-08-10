ebcd28c13a3c5090b8381f42291078ac46e555ef
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-10 20:17:22 +0100

    adiconabdo component renderHistorico no APP

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import userImagem from "../public/Picture.png";
 import pinGift from "../public/pinGift.png";
 import { HiBadgeCheck } from "react-icons/hi";
 import { IoGiftOutline } from "react-icons/io5";
-import { BsPatchCheck } from "react-icons/bs";
+import { RenderHistorico } from "./components/renderHistoricos";
 
 function App() {
   return (
@@ -98,77 +98,7 @@ function App() {
           </div>
         </div>
 
-        <div className="flex flex-col gap-4 px-5 pb-5 pt-6 bg-gray-100 rounded-2xl">
-          <div className="flex justify-between">
-            <h2 className="text-gray-500 uppercase font-bold text-sm">
-              Histórico
-            </h2>
-            <span className="text-gray-500 text-xs">7 cortes</span>
-          </div>
-          <div className="h-px bg-gray-300" />
-
-          <ul>
-            <li className="flex justify-between items-center py-2">
-              <div className="space-y-2">
-                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
-                <h3 className="text-gray-500 text-xs">18:30</h3>
-              </div>
-              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
-                <BsPatchCheck className="size-5 text-green-600" />
-              </div>
-            </li>
-
-            <li className="flex justify-between items-center py-2">
-              <div className="space-y-2">
-                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
-                <h3 className="text-gray-500 text-xs">18:30</h3>
-              </div>
-              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
-                <BsPatchCheck className="size-5 text-green-600" />
-              </div>
-            </li>
-
-            <li className="flex justify-between items-center py-2">
-              <div className="space-y-2">
-                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
-                <h3 className="text-gray-500 text-xs">18:30</h3>
-              </div>
-              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
-                <BsPatchCheck className="size-5 text-green-600" />
-              </div>
-            </li>
-
-            <li className="flex justify-between items-center py-2">
-              <div className="space-y-2">
-                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
-                <h3 className="text-gray-500 text-xs">18:30</h3>
-              </div>
-              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
-                <BsPatchCheck className="size-5 text-green-600" />
-              </div>
-            </li>
-
-            <li className="flex justify-between items-center py-2">
-              <div className="space-y-2">
-                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
-                <h3 className="text-gray-500 text-xs">18:30</h3>
-              </div>
-              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
-                <BsPatchCheck className="size-5 text-green-600" />
-              </div>
-            </li>
-
-            <li className="flex justify-between items-center py-2">
-              <div className="space-y-2">
-                <h2 className="text-gray-600 text-sm">29/04/2024</h2>
-                <h3 className="text-gray-500 text-xs">18:30</h3>
-              </div>
-              <div className="w-8 h-8 p-1 rounded-full bg-green-100 flex items-center justify-center">
-                <BsPatchCheck className="size-5 text-green-600" />
-              </div>
-            </li>
-          </ul>
-        </div>
+        <RenderHistorico />
       </div>
     </div>
   );
