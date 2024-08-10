a7ed3d55233c5e6fe5679c4ebc05af8104a1e657
César Filipe António <cesarfilipeantonio2404@gmail.com>
2024-08-10 17:22:17 +0100

    adicionei paddigton bottom na div mae

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { IoGiftOutline } from "react-icons/io5";
 
 function App() {
   return (
-    <div className="px-3">
+    <div className="px-3 pb-4">
       <div className="h-28 flex items-center">
         <img src={logo} alt="logo imagem" className="w-24 h-12 mx-auto" />
       </div>
@@ -80,12 +80,14 @@ function App() {
         </div>
         <div className="h-24 rounded-2xl py-4 px-5 bg-gray-100 flex gap-6 items-center justify-between">
           <div className="w-full">
-            <h3 className="text-gray-500 text-sm">
-              <span className="text-gray-600 font-bold">3 </span>
+            <h3 className="text-gray-500 text-sm flex gap-2">
+              <span className="text-gray-600 font-bold">3</span>
               cortes restantes
             </h3>
             <div className="flex items-center gap-3">
-              <div className="h-2 bg-gray-300 rounded-full flex-1"></div>
+              <div className="h-2 bg-gray-300 rounded-full flex-1">
+                <span></span>
+              </div>
               <span className="text-gray-500 text-xs">7 de 10</span>
             </div>
           </div>
